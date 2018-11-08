5b6878138302391275c1efc979b060a702986341
the3dsandwich <duhweiwei1013@gmail.com>
2018-11-08 12:59:51 +0800

    finished logic and preparing text change

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ import {
   FormControl,
   Input,
   InputLabel,
-  Avatar,
   Paper
 } from "@material-ui/core";
-import LockIcon from "@material-ui/icons/LockOutlined";
 import MemberData from "./components/member_data_display";
 import "./App.css";
 
@@ -19,9 +17,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      buttonText: "get data",
       email: "",
       name: "",
+      member: null,
       msg: "請輸入姓名及email來看會員編號唷"
     };
     this.database = firebase.initializeApp(firebase_config).database();
@@ -35,91 +33,102 @@ class App extends Component {
     e.preventDefault();
     this.setState({ member: null });
     this.database
-      .ref("/members")
-      .orderByChild("ChiName")
-      .equalTo(this.state.name)
+      .ref("members")
+      .child(this.state.name)
+      .child("ChiName")
       .once("value")
       .then(data => {
-        let searching = data.val();
-        if (searching === null)
-          this.setState({
-            msg: "請再確認你的姓名及email有沒有打錯，或聯絡留港莊員"
-          });
-        else {
-          searching = searching[Object.keys(searching)[0]];
-          var flag = true;
-          searching.Email.forEach(email => {
-            if (this.state.email === email) {
-              this.setState({
-                msg:
-                  "你的會員編號是 " +
-                  searching.MemberID +
-                  "，請確認以下資料有無需要更改的地方！"
-              });
-              this.setState({ member: searching });
-              flag = false;
-            }
-          });
-          if (flag) {
-            this.setState({
-              msg: "請再確認你的姓名及email有沒有打錯，或聯絡留港莊員"
+        if (data.val()) {
+          this.database
+            .ref("members")
+            .child(this.state.name)
+            .child("Email")
+            .once("value")
+            .then(data => {
+              if (data.val() === this.state.email) {
+                // 名字與email都對
+                this.setState({
+                  msg: this.state.name + "您好！"
+                });
+                this.database
+                  .ref("members")
+                  .child(this.state.name)
+                  .once("value")
+                  .then(data => this.setState({ member: data.val() }));
+              } else {
+                // 名字對email錯
+                this.setState({
+                  msg:
+                    this.state.name +
+                    "您好！不好意思，您有在此登錄為會員，但因您的email不符合，故我們無法給您看您的資料，請確認您輸入的email是正確的，謝謝！"
+                });
+              }
             });
-            console.log(searching);
-          } else {
-          }
+        } else {
+          // 名字錯
+          this.setState({
+            msg:
+              "不好意思，您似乎沒有在我們這裡登錄為會員，請再次確認您的姓名有無打錯（或曾改名改姓）"
+          });
         }
       });
   };
 
   render() {
     return (
-      <Grid
-        container
-        alignItems="center"
-        justify="center"
-        spacing={24}
-        direction="column"
-      >
+      <Grid container alignItems="center" justify="center" direction="column">
         <Grid item xs={12} md={6}>
           <Paper className="paper">
-            <Avatar className="avatar">
-              <LockIcon />
-            </Avatar>
-            <Typography variant="display1">
-              留港臺灣學生會會員資料確認
-            </Typography>
+            <Grid container>
+              <Grid item xs={12}>
+                <Typography variant="display1" align="center">
+                  留港臺灣學生會會員資料確認
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <form className="form">
+                  <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="email">中文全名</InputLabel>
+                    <Input
+                      id="name"
+                      name="name"
+                      autoFocus
+                      onChange={this.onSearchChange}
+                    />
+                  </FormControl>
+                  <FormControl margin="normal" required fullWidth>
+                    <InputLabel htmlFor="email">Email Address</InputLabel>
+                    <Input
+                      id="email"
+                      name="email"
+                      onChange={this.onSearchChange}
+                    />
+                  </FormControl>
 
-            <form className="form">
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">中文全名</InputLabel>
-                <Input
-                  id="name"
-                  name="name"
-                  autoFocus
-                  onChange={this.onSearchChange}
-                />
-              </FormControl>
-              <FormControl margin="normal" required fullWidth>
-                <InputLabel htmlFor="email">Email Address</InputLabel>
-                <Input id="email" name="email" onChange={this.onSearchChange} />
-              </FormControl>
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="raised"
-                color="secondary"
-                className="submit"
-                onClick={this.onSearchClick}
-              >
-                Submit
-              </Button>
-            </form>
-            <Typography variant="body1">{this.state.msg}</Typography>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="raised"
+                    color="secondary"
+                    className="submit"
+                    onClick={this.onSearchClick}
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography variant="body1" align="center">
+                  {this.state.msg}
+                </Typography>
+              </Grid>
+            </Grid>
           </Paper>
         </Grid>
 
-        {this.state.member ? <MemberData member={this.state.member} /> : null}
+        {this.state.member ? (
+          <MemberData member={this.state.member} database={this.database} />
+        ) : null}
       </Grid>
     );
   }
--- src/components/member_data_display.js
+++ src/components/member_data_display.js
@@ -2,39 +2,192 @@ import React, { Component } from "react";
 import { Paper, Typography, Grid } from "@material-ui/core";
 
 export default class MemberData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.member;
+  }
+
+  componentDidMount = () => {
+    if (
+      this.props.member.EngName === "" ||
+      this.props.member.Phone === "" ||
+      this.props.member.School === "" ||
+      this.props.member.GradYear === 0 ||
+      this.props.member.GradYear === "" ||
+      this.props.member.Sex === ""
+    ) {
+      this.setState({ complete: false });
+    } else {
+      this.setState({ complete: true });
+      this.props.database
+        .ref("company")
+        .once("value")
+        .then(data => {
+          this.setState({
+            companyName: data.val().name,
+            companyCode: data.val().key
+          });
+        });
+    }
+    if (this.props.member.EngName === "") {
+      this.setState({ EngName: "你還沒填英文名字唷" });
+    }
+    if (this.props.member.Phone === "") {
+      this.setState({ Phone: "你還沒填手機唷" });
+    }
+    if (this.props.member.School === "") {
+      this.setState({ School: "你還沒填學校唷" });
+    }
+    if (this.props.member.GradYear === 0 || this.state.GradYear === "") {
+      this.setState({ GradYear: "你還沒填畢業年唷" });
+    }
+    if (this.props.member.Sex === "") {
+      this.setState({ Sex: "你還沒填性別唷" });
+    }
+    this.setState({
+      Sex:
+        this.props.member.Sex === "M"
+          ? "男"
+          : this.props.member.Sex === "F"
+          ? "女"
+          : this.state.Sex === "你還沒填性別唷" || ""
+          ? this.props.member.Sex
+          : "其他"
+    });
+  };
+
   render() {
     return (
       <Grid item xs={12} md={6}>
-        <Paper className="paper">
-          <Typography variant="title">
-            {this.props.member.ChiName} {this.props.member.EngName}
-          </Typography>
-          {this.props.member.Email.map(email => (
-            <Typography variant="caption">{email}</Typography>
-          ))}
-          <Typography variant="subheading">
-            會員編號：
-            {this.props.member.MemberID}
-          </Typography>
-          <Typography variant="subheading">
-            性別：
-            {this.props.member.Sex === "M"
-              ? "男"
-              : this.props.member.Sex === "F"
-                ? "女"
-                : "你還⋯還沒填"}
-          </Typography>
-          <Typography variant="subheading">
-            學校：
-            {this.props.member.School}
-          </Typography>
-          <Typography variant="subheading">
-            畢業年：
-            {this.props.member.GradYear === 0
-              ? "N/A"
-              : this.props.member.GradYear}
-          </Typography>
+        <Paper className="paper main">
+          <Grid container direction="row" alignItems="center" spacing={8}>
+            <Grid item xs={6}>
+              <Typography variant="display2" align="right">
+                {this.state.ChiName}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="caption">{this.state.EngName}</Typography>
+              <Typography variant="caption">{this.state.Email}</Typography>
+            </Grid>
+          </Grid>
+          <hr />
+          <Grid container direction="row" alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1" align="right">
+                手機：
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">{this.state.Phone}</Typography>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1" align="right">
+                會員編號：
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">
+                {this.state.complete
+                  ? this.state.MemberID
+                  : "請您到下面連結補齊資料拜託"}
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1" align="right">
+                性別：
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">{this.state.Sex}</Typography>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1" align="right">
+                學校：
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">{this.state.School}</Typography>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" alignItems="center">
+            <Grid item xs={6}>
+              <Typography variant="body1" align="right">
+                畢業年：
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">{this.state.GradYear}</Typography>
+            </Grid>
+          </Grid>
         </Paper>
+
+        {this.state.complete ? (
+          <Paper className="paper">
+            <Typography variant="title" align="center">
+              資料齊全！
+              <hr />
+            </Typography>
+            <Typography variant="body1">
+              謝謝您完成我們的資料驗證作業！留港臺灣學生會衷心感謝您的幫助，也請您未來多多支持我們留港的活動與福利！
+            </Typography>
+            {this.state.GradYear > 2017 ? (
+              <div>
+                <hr />
+                <Typography variant="body1">
+                  以下是您的留港機票優惠帳號密碼，此組帳密僅供留港會員使用，請小心收著別被人看到啦！
+                </Typography>
+                <Grid container direction="row" alignItems="center">
+                  <Grid item xs={6}>
+                    <Typography variant="body1" align="right">
+                      {this.state.companyName ? "帳號：" : "loading..."}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      {this.state.companyName ? this.state.companyName : null}
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                <Grid container direction="row" alignItems="center">
+                  <Grid item xs={6}>
+                    <Typography variant="body1" align="right">
+                      {this.state.companyCode ? "密碼：" : "loading..."}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Typography variant="body1">
+                      {this.state.companyCode ? this.state.companyCode : null}
+                    </Typography>
+                  </Grid>
+                </Grid>
+              </div>
+            ) : null}
+          </Paper>
+        ) : (
+          <Paper className="paper">
+            <Typography variant="title" align="center">
+              資料未完成
+            </Typography>
+            <hr />
+            <Typography variant="body1">
+              請您到
+              <a href="https://goo.gl/forms/O0XUdM1jQWcj5JMT2">這個表單</a>
+              填寫資料，我們將盡快為您更新！
+            </Typography>
+          </Paper>
+        )}
       </Grid>
     );
   }
